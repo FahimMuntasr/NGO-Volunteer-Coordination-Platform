7bc4d3e22d2b95bee6b02679de1f38b7ba5a94e8
SianKhanA <siankhan19@gmail.com>
2026-08-05 17:18:58 +0600

    Restrict event management in Django admin

--- events/admin.py
+++ events/admin.py
@@ -1,9 +1,37 @@
 from django.contrib import admin
 
+from accounts.models import User
 from .models import Event, Registration, Team, TeamMembership
 
 
-admin.site.register(Event)
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "ngo",
+        "start_date",
+        "status",
+    )
+
+    def has_add_permission(self, request):
+        return (
+            request.user.is_superuser
+            or request.user.role == User.Role.NGO_ADMIN
+        )
+
+    def has_change_permission(self, request, obj=None):
+        return (
+            request.user.is_superuser
+            or request.user.role == User.Role.NGO_ADMIN
+        )
+
+    def has_delete_permission(self, request, obj=None):
+        return (
+            request.user.is_superuser
+            or request.user.role == User.Role.NGO_ADMIN
+        )
+
+
 admin.site.register(Registration)
 admin.site.register(Team)
 admin.site.register(TeamMembership)
