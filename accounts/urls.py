212c3636de8bc7eb5c35d66f939465f7c92312f8
SianKhanA <siankhan19@gmail.com>
2026-08-12 17:16:24 +0600

    Fixed minor issues

--- accounts/urls.py
+++ accounts/urls.py
@@ -12,7 +12,7 @@ from .views import (
 urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
     path("me/", CurrentUserView.as_view(), name="current-user"),
-    path("register/",RegisterView.as_view(), name="register"),
-    path("password-reset/",PasswordResetRequestView.as_view(),name="password-reset"),
-    path("password-reset/confirm/<str:uidb64>/<str:token>/",PasswordResetConfirmView.as_view(),name="password-reset-confirm"),
+    path("register/", RegisterView.as_view(), name="register"),
+    path("password-reset/", PasswordResetRequestView.as_view(),name="password-reset"),
+    path("password-reset/confirm/<str:uidb64>/<str:token>/", PasswordResetConfirmView.as_view(),name="password-reset-confirm"),
 ]
